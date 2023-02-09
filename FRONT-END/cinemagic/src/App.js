f8bc0a0f8b636cbf7c4bf6a2dc1e3c265eb9a6b5
vermout94 <david.berger4494@gmail.com>
2023-02-09 09:26:39 +0100

    fixed test routing to connect data from backend->frontend

--- FRONT-END/cinemagic/src/App.js
+++ FRONT-END/cinemagic/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+/* import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -18,6 +18,69 @@ function App() {
           Learn React
         </a>
       </header>
+    </div>
+  );
+}
+
+export default App;
+
+*/
+/*
+import React from "react";
+
+
+
+export default function App() {
+
+  let handleClick = async event =>{
+    let response = await fetch('users',{
+      headers:{
+        "Content-Type": "application/json",
+      },
+      method: "get",
+    })
+    let users = await response.json()
+    createUser(users, event.target)
+  }
+
+  let createUser = function (users, button){
+    let nameButton = document.getElementById("clickButton")
+    users.forEach(user =>{
+      let userItem = document.createElement("h2")
+      userItem.innerText = user.name
+      nameButton.appendChild(userItem)
+    })
+
+
+  return (
+    <div>
+        <button id="clickButton" onClick={handleClick}>Test</button>
+    </div>
+  );
+  }}
+  */
+
+
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [stringData, setStringData] = useState("");
+  
+
+  useEffect(() => {
+    async function fetchData() {
+      console.log("test");
+      const response = await fetch("http://localhost:8080/cinemagic/test");
+      const data = await response.text();
+      console.log(data);
+      setStringData(data);
+    }
+    fetchData();
+  },[]);
+
+  return (
+    <div>
+      <p>{stringData}</p>
     </div>
   );
 }
