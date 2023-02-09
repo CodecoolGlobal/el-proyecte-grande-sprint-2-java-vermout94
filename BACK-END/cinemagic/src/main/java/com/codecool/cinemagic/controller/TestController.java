ceb785a2e84a0bda1ab2bf687495244caddc77e0
vermout94 <david.berger4494@gmail.com>
2023-02-09 09:19:20 +0100

    fixed test routing

---
+++ BACK-END/cinemagic/src/main/java/com/codecool/cinemagic/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.codecool.cinemagic.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/cinemagic")
+public class TestController {
+
+    @GetMapping("/test")
+    public String index() {
+        String message = "TEST";
+        return message;
+    }
+}
