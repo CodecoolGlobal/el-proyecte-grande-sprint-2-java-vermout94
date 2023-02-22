ded093e255482956528f32f3a8e9cd971e5037f5
abctim <timbella.xyz@gmail.com>
2023-02-22 14:33:27 +0100

    Added api GetLatestMovies.js and homepage is now displaying the latest movies

--- FRONT-END/cinemagic/src/components/api/GetLatestMovies.js
+++ FRONT-END/cinemagic/src/components/api/GetLatestMovies.js
@@ -1,25 +1,22 @@
 import React, {useEffect, useState} from 'react';
 import Home from "../home/Home";
 
-function GetLatestMovies(props) {
+function GetLatestMovies() {
     const [jsonData, setJsonData] = useState({});
 
     useEffect(() => {
         async function fetchData() {
-            console.log("fetch is working")
             const response = await fetch("http://localhost:8080/cinemagic/latest-movies");
             const data = await response.json();
             setJsonData(data);
         }
-        console.log("fetchData")
         fetchData();
     }, []);
-    console.log(jsonData)
-    console.log("test")
 
+    const latestMovies = jsonData.results;
     return (
         <div>
-            <Home data={jsonData} />
+            <Home data={latestMovies} />
         </div>
     );
 }
