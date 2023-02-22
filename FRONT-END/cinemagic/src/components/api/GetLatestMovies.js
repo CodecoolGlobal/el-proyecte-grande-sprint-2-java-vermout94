018971fda2ebcbe30dadd027a31d5740debabca6
abctim <timbella.xyz@gmail.com>
2023-02-22 20:07:15 +0100

    Home displays now a carousel of the latest movies.Still needs work in CSS plus some restructuring of the whole project

--- FRONT-END/cinemagic/src/components/api/GetLatestMovies.js
+++ FRONT-END/cinemagic/src/components/api/GetLatestMovies.js
@@ -14,6 +14,7 @@ function GetLatestMovies() {
     }, []);
 
     const latestMovies = jsonData.results;
+
     return (
         <div>
             <Home data={latestMovies} />
--- FRONT-END/cinemagic/src/components/home/Home.js
+++ FRONT-END/cinemagic/src/components/home/Home.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Carousel from 'react-material-ui-carousel';
+import { Paper } from '@mui/material';
+import "./Home.css"
+
 
 function Home(props) {
 
     const { data } = props;
-
+    const posterUrl = "https://www.themoviedb.org/t/p/w220_and_h330_face";
+    const backgroundUrl = "https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces";
     return (
-        <div>
+/*        <div>
             <h2>Latest Movies</h2>
             <ul>
                 {data && data.map(movie => (
@@ -15,6 +20,33 @@ function Home(props) {
                     </li>
                 ))}
             </ul>
+        </div>*/
+
+        <div className ='movie-carousel-container'>
+            <Carousel>
+                {
+                    data && data.map(movie =>{
+                        return(
+                            <Paper key={movie.id}>
+                                <div className = 'movie-card-container'>
+                                    {/*<div className="movie-card" style={{"--img": `url(${movie.backdrops[0]})`}}>*/}
+                                    <div className="movie-card">
+                                        <img src={`${backgroundUrl}${movie.backdrop_path}`} alt="" />
+                                        <div className="movie-detail">
+                                            <div className="movie-poster">
+                                                <img src={`${posterUrl}${movie.poster_path}`} alt="" />
+                                            </div>
+                                            <div className="movie-title">
+                                                <h4>{movie.title}</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Paper>
+                        )
+                    })
+                }
+            </Carousel>
         </div>
     );
 }
