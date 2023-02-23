980cb2f8cc488105c2921b7ec0b43dd5ea671b2a
vermout94 <david.berger4494@gmail.com>
2023-02-23 13:45:58 +0100

    Changes to backend - added routes + changed ApiCall - added Signup controller, service, request - added AppUser - application.yml used and env var declared

--- BACK-END/cinemagic/src/main/java/com/codecool/cinemagic/controller/MovieController.java
+++ BACK-END/cinemagic/src/main/java/com/codecool/cinemagic/controller/MovieController.java
@@ -1,24 +1,52 @@
 package com.codecool.cinemagic.controller;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-
 @CrossOrigin
 @RestController
+@Component
 @RequestMapping("/cinemagic")
 public class MovieController {
 
-        @GetMapping("/latest-movies")
-        public String index() {
-            RestTemplate restTemplate = new RestTemplate();
-            String api_key = "6c04a613f1bf8ff8766981ccebbb11df";
-            String uri = "https://api.themoviedb.org/3/movie/now_playing?api_key={api_key}&language=en-US&page=1";
-            String response = restTemplate.getForObject(uri, String.class, api_key);
-            return response;
-            // changes to push idea changes
-        }
+    private final String apiKey;
+
+    public MovieController(String apiKey) {
+        this.apiKey = apiKey;
     }
+
+    @GetMapping("/latest-movies")
+    public String latestMovies() {
+        String uri = "https://api.themoviedb.org/3/movie/now_playing?api_key={apiKey}&language=en-US&page=1";
+        return doApiCall(uri);
+    }
+
+    @GetMapping("/top-rated-movies")
+    public String topRatedMovies() {
+        String uri = "https://api.themoviedb.org/3/movie/top_rated?api_key={apiKey}&language=en-US&page=1&region=DE";
+        return doApiCall(uri);
+    }
+
+
+    @GetMapping("/most-popular-movies")
+    public String mostPopularMovies() {
+        String uri = "https://api.themoviedb.org/3/movie/popular?api_key={apiKey}&language=en-US&page=1&region=DE";
+        return doApiCall(uri);
+    }
+
+    //TODO: add fetch data from react and post it to the backend, really needed?
+    /*@GetMapping("/movie/{movie_Id}"){
+        RestTemplate restTemplate = new RestTemplate();
+        String uri = "https://api.themoviedb.org/3/movie/{movie_id}?api_key={apiKey}&language=en-US";
+        return restTemplate.getForObject(uri, String.class, apiKey, movie_Id);
+    }*/
+
+    public String doApiCall(String uri) {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(uri, String.class, apiKey);
+    }
+}
--- FRONT-END/cinemagic/src/App.js
+++ FRONT-END/cinemagic/src/App.js
@@ -3,6 +3,7 @@ import {Routes, Route} from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./components/home/Home";
 import Header from "./components/header/Header";
+import GetLatestMovies from "./components/home/GetLatestMovies";
 
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
             <Header/>
             <Routes>
                 <Route path="/" element={<Layout/>}>
-                    <Route path="/" element={<Home/>}></Route>
+                    {/*<Route path="/" element={<Home/>}></Route>*/}
+                    <Route path="/" element={<GetLatestMovies/>}></Route>
                 </Route>
             </Routes>
         </div>
