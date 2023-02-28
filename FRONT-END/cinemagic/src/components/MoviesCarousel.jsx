d10591cdff199081b43acf7aad6d4366a1c68781
abctim <timbella.xyz@gmail.com>
2023-02-28 12:13:13 +0100

    Changed the carousel from MUI to react-bootstrap. Still needs some styling done

--- FRONT-END/cinemagic/src/components/MoviesCarousel.jsx
+++ FRONT-END/cinemagic/src/components/MoviesCarousel.jsx
@@ -1,45 +1,49 @@
 import React from 'react';
-import Carousel from 'react-material-ui-carousel';
-import { Paper } from '@mui/material';
-import "./MovieCarousel.css"
-
+import { Carousel } from 'react-bootstrap';
 
 function MoviesCarousel(props) {
-
     const { data } = props;
     const posterUrl = "https://www.themoviedb.org/t/p/w220_and_h330_face";
     const backgroundUrl = "https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces";
 
     return (
-        <div className ='movie-carousel-container'>
-            <h2 style={{color: "white"}}>Featured movies</h2>
+        <div>
+            <h2 style={{ color: "white" }}>Latest movies</h2>
+
             <Carousel>
-                {
-                    data && data.map(movie =>{
-                        return(
-                            <Paper key={movie.id}>
-                                <div className = 'movie-card-container'>
-                                    <div className="movie-card">
-                                        <img src={`${backgroundUrl}${movie.backdrop_path}`} alt="" />
-                                        <div className="movie-detail">
-                                            <div className="movie-poster">
-                                                <a href={"/" + movie.title}>
-                                                    <img src={`${posterUrl}${movie.poster_path}`} alt="" />
-                                                </a>
-                                            </div>
-                                            <div className="movie-title">
-                                                <h4>{movie.title}</h4>
-                                            </div>
-                                        </div>
-                                    </div>
+                {data &&
+                    data.map((movie) => {
+                        return (
+                            <Carousel.Item key={movie.id}>
+                                <div
+                                    style={{
+                                        position: "relative",
+                                        height: "100%",
+                                        display: "flex",
+                                        justifyContent: "center",
+                                        alignItems: "center",
+                                    }}
+                                >
+                                <img
+                                    className="d-block w-100"
+                                    src={`${backgroundUrl}${movie.backdrop_path}`}
+                                    alt={movie.title}
+                                />
+                                <Carousel.Caption>
+                                    <img
+                                        src={`${posterUrl}${movie.poster_path}`}
+                                        alt={`${movie.title} poster`}
+                                        style={{ maxWidth: "400px", border: "solid yellow 4px", borderRadius: "10px", }}
+                                    />
+                                    <h3>{movie.title}</h3>
+                                </Carousel.Caption>
                                 </div>
-                            </Paper>
-                        )
-                    })
-                }
+                            </Carousel.Item>
+                        );
+                    })}
             </Carousel>
         </div>
     );
 }
 
-export default MoviesCarousel
+export default MoviesCarousel;
