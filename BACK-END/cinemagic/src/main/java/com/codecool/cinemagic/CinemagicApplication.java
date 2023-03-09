8ac1019677e8a75e4cbd6a80aa1e3fd9ff5f4979
abctim <timbella.xyz@gmail.com>
2023-03-09 15:37:14 +0100

    Passwords from users are now encrypted.

--- BACK-END/cinemagic/src/main/java/com/codecool/cinemagic/CinemagicApplication.java
+++ BACK-END/cinemagic/src/main/java/com/codecool/cinemagic/CinemagicApplication.java
@@ -2,9 +2,8 @@ package com.codecool.cinemagic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication/*(exclude = {DataSourceAutoConfiguration.class })*/	//TODO: delete "exclude", when database is ready
+@SpringBootApplication
 public class CinemagicApplication {
 
 	public static void main(String[] args) {
--- BACK-END/cinemagic/src/main/java/com/codecool/cinemagic/communication/controller/AppUserController.java
+++ BACK-END/cinemagic/src/main/java/com/codecool/cinemagic/communication/controller/AppUserController.java
@@ -2,6 +2,7 @@ package com.codecool.cinemagic.communication.controller;
 import com.codecool.cinemagic.model.AppUser;
 import com.codecool.cinemagic.repository.AppUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,29 +11,12 @@ import org.springframework.web.bind.annotation.*;
 public class AppUserController {
     @Autowired
     private AppUserRepository appUserRepository;
-
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
     @PostMapping("signup")
     public AppUser signup(@RequestBody AppUser appUser) {
+        String encodedPassword = bCryptPasswordEncoder.encode(appUser.getPassword());
+        appUser.setPassword(encodedPassword);
         return appUserRepository.save(appUser);
     }
-
-   /* This is just needed reference for later
-   @PostMapping("/signup")
-    public ResponseEntity<AppUser> signup(@RequestBody AppUser user) {
-        // Check if the username already exists
-        Optional<AppUser> existingUser = appUserRepository.findByUsername(user.getUsername());
-        if (existingUser.isPresent()) {
-            throw new ResponseStatusException(HttpStatus.CONFLICT, "Username already exists");
-        }
-
-        // Encrypt the password
-        String hashedPassword = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt());
-        user.setPassword(hashedPassword);
-
-        // Save the user to the database
-        AppUser savedUser = appUserRepository.save(user);
-
-        // Return the saved user object in the response body
-        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
-    }*/
 }
