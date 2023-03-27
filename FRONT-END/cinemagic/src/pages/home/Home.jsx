842827f229dd7961fa0c74966a9ef020b72a69aa
vermout94 <david.berger4494@gmail.com>
2023-03-27 14:39:21 +0200

    changed header to change the login logout buttons

--- FRONT-END/cinemagic/src/pages/home/Home.jsx
+++ FRONT-END/cinemagic/src/pages/home/Home.jsx
@@ -119,7 +119,7 @@ export default function Home() {
                     setIsLoggedIn(true);
                 }
             } catch (error) {
-                console.error(error);
+                console.error("Error with token: ",error);
             }
         }
     }, []);
